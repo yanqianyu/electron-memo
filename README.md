002f0d4b69da325814bb1859d0c580e8bea61d80
YanQianyu <284989344@qq.com>
2020-08-05 10:55:47 +0800

    init proj

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # electron-memo
 A tiny memo based on Electron8 and Vue
+
+使用vue + electron + nodejs写的便签应用
+
+[ ] nodejs写入本地数据库
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
---
+++ src/components/Note.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="bigbox">
+        <!-- 搜索框 -->
+        <input type="text" value="" placeholder="What needs to be done?" v-model="task" v-on:keydown.enter="addTodo">
+        <ul>
+            <li v-for="(todo, key) in todos" v-bind:key="key">
+                <div>
+                    <!-- checkbox绑定todo的状态 -->
+                    <input type="checkbox" v-model="todo.status" v-on="toggleTaskStatus(key)">
+                    <span>{{todo.content}}</span>
+                    <!-- 删除todo事件 -->
+                    <button v-on:click="deleteTodo(key)"></button>
+                </div>
+            </li>
+        </ul>
+        <!-- 选项卡 -->
+        <div>
+            <div>{{todoNums}} items left</div>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Note",
+    data: function() {
+        return {
+            todos: [],
+            task: "",
+            todoNums: 0
+        }
+    },
+    created: function() {
+        // 从后台获取全部task
+        this.todoNums = this.todos.length;
+    },
+    methods: {
+        toggleTaskStatus: function(key) {
+            this.todos[key].status = !this.todos[key].status;
+        },
+        addTodo: function() {
+            // 确保输入有内容
+            console.log(this.task);
+            if (this.task.length > 0) {
+                var newTodo = {};
+                newTodo.content = this.task;
+                newTodo.status = false;
+                this.todos.push(newTodo);
+                // 清空文本框
+                this.task = "";
+                this.todoNums = this.todos.length;
+            }
+        },
+        deleteTodo: function(key) {
+            // 删除任务
+            // 响应式原理
+            this.todos.splice(key, 1);
+            this.todoNums = this.todos.length;
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
